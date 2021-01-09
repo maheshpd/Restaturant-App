cfb4e2a8ddb37ffe7f7bad42373fa1ff40a49138
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-09 21:11:34 +0530

    Initial Commit

---
+++ Restaturant App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaturant App
+//
+//  Created by Mahesh Prasad on 09/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restaturant App/Restaturant_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaturant_AppApp.swift
+//  Restaturant App
+//
+//  Created by Mahesh Prasad on 09/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Restaturant_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
